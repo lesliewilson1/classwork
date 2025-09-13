82ad4e8fe5a38417bf56f72c95acfffd3b01c208
Leslie <wilson.leslie2021@gmail.com>
2025-09-12 21:49:06 -0400

    Adding files to Repo

---
+++ Morning Activities/308_JavaExercise/Exercise.js
@@ -0,0 +1,46 @@
+//Find the Sum
+let apple = 1;
+let grape = 2;
+
+let fruit = apple + grape;
+
+console.log (fruit)
+
+//Find the Sum 2
+
+let pineapple = 4;
+let banana = 5;
+let smoothie = 0;
+
+smoothie += fruit + pineapple + banana;
+
+console.log (smoothie)
+
+//Find the Average
+
+let black = 0;
+let red = 0;
+let yellow = 0;
+let green = 0;
+let purple = 0;
+let brown = 0;
+let blue = 0;
+let pink = 0;
+let white = 0;
+
+let sum1 = 0;
+let sum2 = 0;
+
+sum1 += black + red + yellow;
+sum1 += purple + brown + blue;
+
+sum1/=5;
+sum2/=5;
+
+let avr = (sum1+sum2)/ 2
+console.log (avr)
+
+//Exercise 4 - bascially copy from above
+
+let box
+
